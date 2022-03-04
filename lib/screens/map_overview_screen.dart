05651b9ec059805a774fb309605e9900d83f0698
David Corrigan <davidcorrigan@Davids-iMac.local>
2022-03-04 00:22:39 +0000

    removing unused import

--- lib/screens/map_overview_screen.dart
+++ lib/screens/map_overview_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:firebase_auth/firebase_auth.dart';
